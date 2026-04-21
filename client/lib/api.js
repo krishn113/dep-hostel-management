d5547f7f2c865cc7d577bdf19b57407a700dc218
krishn113 <ag.krishna0807@gmail.com>
2026-04-21 16:13:56 +0530

    backend-deploy-fix-2

--- client/lib/api.js
+++ client/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://dep-hostel-management.onrender.com/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://dep-hostel-management.onrender.com/api",
   withCredentials: true
 });
 
